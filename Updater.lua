c204f119fed1e37bcfbc75083415d1629fde35c1
Super <38338700+superpowers04@users.noreply.github.com>
2021-05-13 14:46:43 -0400

    Add more error checking and clean up some things

--- Updater.lua
+++ Updater.lua
@@ -8,41 +8,48 @@ local debugmode = false -- Change to true to disable updating
 local f = string.format
 -- print('Checking for updates...')
 local iswindows = true
-if io.open('/dev','r') then
-	iswindows = false
-end
+if io.open('/dev','r') then iswindows = false end
 function curl(url)
 	if iswindows then -- Windows
-		return assert(io.popen(f('./cURL/curl.exe %q',url),'r'):read('*a'))
+		return assert(io.popen(f('cURL/curl.exe -s %q',url),'r'):read('*a'))
 	else -- Unix
-		return assert(io.popen(f('curl %q',url),'r'):read('*a'))
+		return assert(io.popen(f('curl -s %q',url),'r'):read('*a'))
 	end
+end
+function clear() -- Clears the command prompt/window
+	if iswindows then return os.execute('cls') else return os.execute('clear') end
 end
 local verfile = io.open('version','r')
 version = ' Not installed!'
 if verfile then version = verfile:read('*a') end
 
 
-function update() 
+function update() -- Check for updates and update if needed 
 	
 	if not debugmode then -- Don't execute
 		local versionurl = "https://raw.githubusercontent.com/superpowers04/ChartAdderForFunkinMulti/main/version" -- Change this to your URL if you're forking it
 		print('Checking for updates...')
-		local onlinever = 0.0
+		local onlinever = 0.0 
 		onlinever = curl(versionurl) -- download and check version from github
+		if onlinever == '' then print('Unable to check for updates!') io.read() return end
 		if onlinever then onlinever = string.gsub(onlinever,'\n','') end
 		if onlinever and onlinever ~= version then
-			print(f('New update available %s! Your version:%s, Would you like to update?\n Type "y" to update or anything else to skip',onlinever,version))
+			print(f('New update available %s! Your version:%s, Would you like to update?\nIf updating does not work, then you can use the alternative download from the gamebanana page\nType "y" and press enter to update or anything else to skip',onlinever,version))
 			answer = string.lower(io.read())
 			if answer and answer == 'y' then 
+				clear()
 				print('Updating...')
 				-- io.open('./version','w'):write(onlinever)
-				local update = curl('https://raw.githubusercontent.com/superpowers04/ChartAdderForFunkinMulti/main/Addnewsongs.lua')
-				-- print(update)
-				print('')
-				local succ,err = pcall(function()load(update)end)
-				if not succ then 
-					print(f('Something went wrong when trying to update! Please report:%s',err))
+				local update = curl('https://raw.githubusercontent.com/superpowers04/ChartAdderForFunkinMulti/main/Addnewsongs.lua') -- Grab latest lua file
+				if update == '' or not string.match(update,'https://shadowmario.itch.io/funkinmult') then -- Make sure download actually contains lua file
+					print(f('Something went wrong when trying to update! I got no response',err))
+					io.read() 
+					return 
+				end
+				
+				local succ,err = pcall(function()load(update)end) -- Run in pcall to catch errors
+				if not succ then -- If there's an error, print it!
+					print(f('Something went wrong when trying to update! Please report:%s\nPress enter to continue',err))
 					io.read() 
 					return 
 				end
@@ -60,7 +67,7 @@ function update()
 				print('Skipping update!')
 			end
 		elseif onlinever and onlinever == version then
-			print('Up to date, Nothing to do')
+			print('Uptodate, Nothing to do')
 		else
 			print('Unable to check for updates!\nPress enter to continue')
 			io.read()
@@ -68,6 +75,5 @@ function update()
 
 	end
 end
--- if not version then version = 0 end
 update()
 dofile('Addnewsongs.lua')
